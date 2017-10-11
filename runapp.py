3b52ecaf9fde080d8a53a03d04a4d4e6c3554024
shd <759275499@qq.com>
2017-10-11 19:09:41 +0800

    新增文件

--- runapp.py
+++ runapp.py
@@ -15,12 +15,13 @@ import swipe
 class TestSequenceFunctions(unittest.TestCase):
 	@classmethod
 	def setUpClass(self):
-		self.driver = driver
+		pass
+		
 	
 		#首页
 	# def test_1shouye(self):
 		# try:
-			# em =self.driver.find_element_by_id('com.sxsfinance.SXS:id/act_product_deails_btn_investment')
+			# em =swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/act_product_deails_btn_investment')
 			# em.click()
 			# sleep(1)
 			# #项目信息
@@ -72,46 +73,65 @@ class TestSequenceFunctions(unittest.TestCase):
 	# 我的
 	def test_3my(self):
 		sleep(2)
+		swipe.driver.implicitly_wait(4)
 		swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'我的')]")
+		
 		try:
-			em =self.driver.find_element_by_id('com.sxsfinance.SXS:id/login_btn')
-			swipe.driver_find_by_id_keys('com.sxsfinance.SXS:id/login_phone',"18701670001")
-			swipe.driver_find_by_id_keys('com.sxsfinance.SXS:id/login_password','123456')
+			em =swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/login_btn')
+			phone = raw_input('enter your phone:')
+			swipe.driver_find_by_id_keys('com.sxsfinance.SXS:id/login_phone',phone)
+			passwd = raw_input('enter your password:')
+			swipe.driver_find_by_id_keys('com.sxsfinance.SXS:id/login_password',passwd)
 			swipe.driver_find_by_id('com.sxsfinance.SXS:id/login_btn')
 		except:
+			
 			#我的资产总额
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_total_sum')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_set_button')
-			# #设置
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_set_button')
-			# #存管按钮
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_deposit')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/mytips_close')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_ca')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/mytips_close')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_bindcard')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/mytips_close')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_total_sum')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_set_button')
+			#设置
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_set_button')
+			#存管按钮
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_deposit')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/mytips_close')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_ca')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/mytips_close')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_bindcard')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/mytips_close')
 
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
-			# #昨日利息
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_left_top')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
-			# #累计利息
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_right_top')
-			# swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
+			#昨日利息
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_left_top')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
+			#累计利息
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_right_top')
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
 			#红包
 			swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'红包金额')]")
 			swipe.swipe_Left(100)
 			swipe.swipe_Right(100)
 			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
-			#红包
+			#邀请
 			swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'邀请返现')]")
 			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
-			#红包
+			#小沙
 			swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'小沙精选记录')]")
 			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
-			#红包
+			#四方化缘
 			swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'四方化缘记录')]")
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
+			#债权出让
+			swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'债权出让')]")
+			swipe.swipe_Left(100)
+			swipe.swipe_Left(100)
+			swipe.swipe_Right(100)
+			swipe.swipe_Right(100)
+			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
+			#资金流水
+			swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'资金流水')]")
+			swipe.swipe_Left(100)
+			swipe.swipe_Left(100)
+			swipe.swipe_Right(100)
+			swipe.swipe_Right(100)
 			swipe.driver_find_by_id('com.sxsfinance.SXS:id/my_return_button')
 			print '333'
 			
@@ -128,23 +148,23 @@ class TestSequenceFunctions(unittest.TestCase):
 	# def test_regist(self):
 		# swipe.driver_find_by_xpath("//android.widget.TextView[contains(@text,'我的')]")
 		# try:
-			# em =self.driver.find_element_by_id('com.sxsfinance.SXS:id/login_btn')
+			# em =swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/login_btn')
 			
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/register_account').click()
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/edttxt_register_phong').send_keys('13521137773')
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/btn_phone_code').click()
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/edttxt_register_code').send_keys('111111')
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/register_account').click()
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/edttxt_register_phong').send_keys('13521137773')
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/btn_phone_code').click()
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/edttxt_register_code').send_keys('111111')
 			# update_db_code()
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/btn_regiter').click()
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/act_update_psd_newps_set_password_edittext').send_keys('123456')
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/act_update_psd_newps_set_new_password_edittext').send_keys('123456')
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/act_update_newps_password_button').click()
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/btn_regiter').click()
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/act_update_psd_newps_set_password_edittext').send_keys('123456')
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/act_update_psd_newps_set_new_password_edittext').send_keys('123456')
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/act_update_newps_password_button').click()
 			# sleep(2)
-			# self.driver.find_element_by_id('com.sxsfinance.SXS:id/act_gesture_verify_tvJumpOver').click()
+			# swipe.driver.find_element_by_id('com.sxsfinance.SXS:id/act_gesture_verify_tvJumpOver').click()
 			# sleep(3)	
 		# except:
 			# print '3333'
-			# self.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'产品')]").click()
+			# swipe.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'产品')]").click()
 	
 
 
@@ -152,7 +172,7 @@ class TestSequenceFunctions(unittest.TestCase):
 	
 	@classmethod
 	def tearDownClass(self):
-		self.driver.quit()
+		swipe.driver.quit()
 	
 
 
@@ -161,7 +181,7 @@ if __name__ =="__main__":
 
 	
 	
-	driver = swipe.driver
+	
 	suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
 	unittest.TextTestRunner(verbosity=2).run(suite)
 	#unittest.main()
